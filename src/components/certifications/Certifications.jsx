23570e4216db238f14c144542cd2eb566586ecee
Newt-Shadow <anmol.s.sahoo@gmail.com>
2024-09-09 19:22:51 +0530

    commit

---
+++ src/components/certifications/Certifications.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import CertificationCard from "../certificationCard/CertificationCard";
+import "./Certifications.css";
+import gcp_logo from '../../assets/images/gcp_logo.png';
+
+
+const blueTheme = {
+  body: "#EDF9FE",
+  text: "#001C55",
+  expTxtColor: "#000a12",
+  highlight: "#A6E1FA",
+  dark: "#00072D",
+  secondaryText: "#7F8DAA",
+  imageHighlight: "#0E6BA8",
+  compImgHighlight: "#E6E6E6",
+  jacketColor: "#0A2472",
+  headerColor: "#0E6BA877",
+  splashBg: "#001C55",
+};
+
+const Certifications = ({ theme = blueTheme }) => {
+  const certifications = {
+    certifications: [
+      {
+        title: "Introduction to Front-End Development",
+        subtitle: "Meta",
+        logo_path: "meta.png",
+        certificate_link: "https://www.coursera.org/account/accomplishments/verify/VSA8TUSH47FR",
+        alt_name: "Meta",
+        color_code: "#fff",
+      },
+      {
+        title: "Programming with JavaScript",
+        subtitle: "Meta",
+        logo_path: "meta.png",
+        certificate_link: "https://www.coursera.org/account/accomplishments/verify/XT40TSDOUJU3",
+        alt_name: "Meta",
+        color_code: "#fff",
+      },
+      {
+        title: "Data Structures and Algorithm",
+        subtitle: "Geeks For Geeks",
+        logo_path: "gfg.png",
+        certificate_link: "",
+        alt_name: "GfG",
+        color_code: "#fff",
+      },
+      {
+        title: "Full Stack Web - Development",
+        subtitle: "Udemy",
+        logo_path: "udemy.png",
+        certificate_link: "",
+        alt_name: "Udemy",
+        color_code: "#fff",
+      },
+      {
+        title: "Kuberenetes on GCP",
+        subtitle: "- Qwiklabs",
+        logo_path: "gcp_logo.png",
+        certificate_link: "",
+        alt_name: "GCP",
+        color_code: "#fff",
+      },
+      {
+        title: "Cloud Architecture",
+        subtitle: "- Qwiklabs",
+        logo_path: gcp_logo,
+        certificate_link: "",
+        alt_name: "GCP",
+        color_code: "#fff",
+      },
+    ],
+  };
+
+  return (
+    <div className="bg-body p-8" id="certs">
+      <div className="mb-6 text-center">
+        <h1 className="text-[40px] leading-[1.1] font-google-sans md:text-[35px] sm:text-[30px]" style={{ color: theme.text }}>
+          Certifications
+        </h1>
+      </div>
+      <div className="certs-body-div">
+        {certifications.certifications.map((cert, index) => (
+          <CertificationCard certificate={cert} theme={theme} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Certifications;
